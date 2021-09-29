8bc054b59d790fce646dc61a896fc9eb16330443
Nick Rice <58044754+nickrice38@users.noreply.github.com>
2021-09-29 09:36:41 +0100

    Add files via upload

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Nick Rice on 20/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
